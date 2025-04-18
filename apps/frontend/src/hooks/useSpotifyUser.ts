2010fa9a2bbb11097ed258ceebcad15e8e5ca0c2
Tech-Soumyo <soumyodeepdutta360@gmail.com>
2025-04-18 22:19:12 +0530

    add getSpotifyUser hook for fetching user data from Spotify API

---
+++ apps/frontend/src/hooks/useSpotifyUser.ts
@@ -0,0 +1,20 @@
+import { SpotifyUserData } from "@/types/user.type";
+import axios from "axios";
+
+const SPOTIFY_API_URL = "https://api.spotify.com/v1/me";
+export const getSpotifyUser = async (
+  accessToken: string
+): Promise<SpotifyUserData> => {
+  try {
+    const response = await axios.get(SPOTIFY_API_URL, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    // console.log("useSpotifyUser.ts", response.data);
+    return response.data; // Axios already parses the response as JSON
+  } catch (error: any) {
+    console.error("Error fetching Spotify user data:", error);
+    throw error; // Re-throw the error to handle it in the calling function
+  }
+};
